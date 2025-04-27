c4720e9c1e2225811ce6354041653ea3b375b016
Atanas Yonkov <atanasyonkov1@gmail.com>
2025-04-27 12:25:20 +0300

    Add simple Styling

--- src/components/BoardList/BoardList.js
+++ src/components/BoardList/BoardList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addBoard } from '../../features/board/boardSlice'
 import Column from './Column'
+import '../../styles/Kanban.css'
 
 const BoardList = () => {
     const dispatch = useDispatch()
